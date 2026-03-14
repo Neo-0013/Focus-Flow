800ff27881214d410be300df93d6e58240b3d880
NEO-013 <nawazrk1413@gmail.com>
2026-03-14 23:50:08 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("focusflow").then(cache => {
+      return cache.addAll(["/", "/index.html", "/assets/css/style.css", "/assets/js/app.js"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
